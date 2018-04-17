ef9045d16d079000fa313f92f841eccebe01044b
diego-maya <38476363+diego-maya@users.noreply.github.com>
2018-04-17 18:32:46 -0500

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -5,7 +5,7 @@ Assignment:         Galaga : Bullet : Header File
 Date Created:       4/3/2018
 Date Last Modified: 4/3/2018 : (Mark)Initial Header variables and functions.
 					4/8/2018 : (Mark)Added moveShip function
-					4/17/2018: (Diego) Added moveBulet
+					
 
 */
 
@@ -33,6 +33,5 @@ public:
     void moveShip(int x, int y);// moves ship x pixels in the x direction and y pixels in the y diirction
     //void draw(pass in array for char pixels);
 
-    void moveBullet();
 };
 
