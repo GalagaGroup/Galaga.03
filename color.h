dfa3174882656b428f57d64f3d474e459a4c099d
diego-maya <38476363+diego-maya@users.noreply.github.com>
2018-04-17 18:20:12 -0500

    Update color.h

--- color.h
+++ color.h
@@ -7,12 +7,13 @@ Date Last Modified: 4/3/2018 : (Mark)Initial Functions and variables
 					4/8/2018 : (Mark)Added getcolor functions
 					4/10/2018 : (Mark) fix of syntax errors and general incorrectness
 					4/17/2018  : (Mark)Debugging
+					4/17/2018  : (Diego) set colors from 'int color = 128;' to 'int color;'
 */
 
 struct Color{
-    int r = 128;
-    int g = 128;
-    int b = 128;
+    int r;
+    int g;
+    int b;
     Color(){
     r = 0;
     g = 0;
